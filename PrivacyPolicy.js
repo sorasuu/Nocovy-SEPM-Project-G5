72fdd53f58810df68a05d693936dadf451f16acf
Lam Nguyen <topovn0908@gmail.com>
2020-04-21 00:14:23 +0700

    rules, faq, tos and policy

---
+++ PrivacyPolicy.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import { Redirect, NavLink } from 'react-router-dom'
+import { firestoreConnect } from 'react-redux-firebase'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
+import { Button, Paper, Grid } from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add';
+import StyledButton from '../layout/StyledButton'
+import ProductDetailCard from '../layout/ProductDetailCard'
+import ColorLinearProgress from '../layout/ColorLinearProgress'
+
+export class TermOfService extends Component {
+    render() {
+        return (
+            <div className="container" style={{ textAlign: 'center' }}>
+                <h3>Nocovy Privacy Policy</h3>
+                <p>This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from Nocovy (the “Site”).</p>
+                <br />
+                <b>Personal information we collect</b>
+                <p>When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically-collected information as “Device Information”. </p>
+                <p>We collect Device Information using the following technologies:
+                <br />
+                - “Cookies” are data files that are placed on your device or computer and often include an anonymous unique identifier. For more information about cookies, and how to disable cookies, visit http://www.allaboutcookies.org. <br />
+                - “Log files” track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps. <br />
+                - “Web beacons”, “tags”, and “pixels” are electronic files used to record information about how you browse the Site. </p>
+
+                <p>Additionally when you make a purchase or attempt to make a purchase through the Site, we collect certain information from you, including your name, billing address, shipping address, payment information (including credit card numbers, email address, and phone number. We refer to this information as “Order Information”.
+                When we talk about “Personal Information” in this Privacy Policy, we are talking both about Device Information and Order Information.</p>
+                <br />
+                <b>How do we use your personal information?</b>
+                <p>We use the Order Information that we collect generally to fulfill any orders placed through the Site (including processing your payment information, arranging for shipping, and providing you with invoices and/or order confirmations). Additionally, we use this Order Information to: <br />
+                - Communicate with you; <br />
+                - Screen our orders for potential risk or fraud; and <br />
+                - When in line with the preferences you have shared with us, provide you with information or advertising relating to our products or services.</p>
+
+                <p>We use the Device Information that we collect to help us screen for potential risk and fraud (in particular, your IP address), and more generally to improve and optimize our Site (for example, by generating analytics about how our customers browse and interact with the Site, and to assess the success of our marketing and advertising campaigns).</p>
+                <br />
+                <b>Sharing you personal Information</b>
+                <p>We share your Personal Information with third parties to help us use your Personal Information, as described above. For example, we use Shopify to power our online store--you can read more about how Shopify uses your Personal Information here: https://www.shopify.com/legal/privacy. We also use Google Analytics to help us understand how our customers use the Site -- you can read more about how Google uses your Personal Information here: https://www.google.com/intl/en/policies/privacy/. You can also opt-out of Google Analytics here: https://tools.google.com/dlpage/gaoptout.</p>
+
+                <p>Finally, we may also share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights.</p>
+                <br />
+                <b>Behavioural advertising</b>
+                <p>As described above, we use your Personal Information to provide you with targeted advertisements or marketing communications we believe may be of interest to you. For more information about how targeted advertising works, you can visit the Network Advertising Initiative’s (“NAI”) educational page at http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work.</p>
+
+                <p>You can opt out of targeted advertising by using the links below:<br />
+                - Facebook: https://www.facebook.com/settings/?tab=ads<br />
+                - Google: https://www.google.com/settings/ads/anonymous<br />
+                - Bing: https://advertise.bingads.microsoft.com/en-us/resources/policies/personalized-ads</p>
+
+                <p>Additionally, you can opt out of some of these services by visiting the Digital Advertising Alliance’s opt-out portal at: http://optout.aboutads.info/.</p>
+                <br />
+                <b>Do not track</b>
+                <p>Please note that we do not alter our Site’s data collection and use practices when we see a Do Not Track signal from your browser.
+                </p>
+                <br />
+                <b>Data retention</b>
+                <p>When you place an order through the Site, we will maintain your Order Information for our records unless and until you ask us to delete this information.</p>
+                <br />
+                <b>Changes</b>
+                <p>We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal or regulatory reasons.</p>
+                <br />
+                <b>Contact us</b>
+                <p>For more information about our privacy practices, if you have questions, or if you would like to make a complaint, please contact us by e‑mail at Nocovy</p>
+                <br /><br />
+            </div>
+
+
+        )
+    }
+}
+
+export default TermOfService
