f623da97a90fa18f7a3733247095ea258934e74b
JDParadox <JamesDParadox@gmail.com>
2020-04-25 22:14:31 +0700

    more product fields

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -8,7 +8,6 @@ import AddProductCard from '../layout/AddProductCard'
 import ProductCard from '../layout/ProductCard'
 import Modal from '@material-ui/core/Modal';
 
-
 class Profile extends Component {
   state={
     open:false,
@@ -20,10 +19,22 @@ class Profile extends Component {
     description: `Originally Established in 1985, Coastal Designs Decor is a long established and experienced Australian wholesale distribution business.
     Our main products are Home decorating accessories in the form of Volkswagen memorabilia, such as VW Beetle and VW Kombi or beach theme and coastal designs like:- Lighthouses Anchors, oars, paddles, beach girl figurines, pelicans, starfish, shells etc.
     Product lines include Photo frames, money boxes, wall plaques, book boxes, trinket boxes, candle holders, tea light holders, wall art including shadow boxes, shell theme products`,
-    title:'',
-    subHeader:'',
-    info:'',
-    price: 0,
+    productName:'',
+    productBrand:'',
+    productOrigin:'',
+    productDesc:'',
+    productCategories: ['',''],
+    dutyCost:0,
+    dutyRate:0,
+    FOB:'',
+    freightCost:0,
+    freightDesc:'',
+    freightRate:'',
+    landedCost:0,
+    margin:'',
+    miscCost:0,
+    unitCost:0,
+    unitPrice:0,
     image: '',
   }
 
@@ -39,7 +50,36 @@ class Profile extends Component {
   handleChange = input => e => {
     this.setState({ [input]: e.target.value })
     console.log(this.state)
-}
+  }
+
+  handleCatChange = input => e => {
+    this.setState({ [input]: e.target.value })
+    console.log(this.state)
+    //Write function to parse string input into array of tags
+  }
+
+  formSubmit = () => {
+    const product = {supplierId: '', 
+                    name: this.state.productName, 
+                    detail: {origin: this.state.productOrigin,
+                            brand: this.state.productBrand}, 
+                    description: this.state.productDesc, 
+                    category: this.state.category, 
+                    price: {dutyCost: this.state.dutyCost,
+                            dutyRate: this.state.dutyRate,
+                            FOB: this.state.FOB,
+                            freightCost: this.state.freightCost,
+                            freightDesc: this.state.freightDesc,
+                            freightRate: this.state.freightRate,
+                            landedCost: this.state.landedCost,
+                            margin: this.state.margin,
+                            miscCost: this.state.miscCost,
+                            unitCost: this.state.unitCost,
+                            unitPrice: this.state.unitPrice}}
+    console.log(product)
+    //Form submit functions go here
+  }
+
 
   render() {
     return (
@@ -59,9 +99,9 @@ class Profile extends Component {
              <ProductCard  product={product}/>
           </Grid>
           )}):<h5>Loading...</h5>}</Grid>
-          <Modal open={this.state.open} onClose={this.handleClose}>
-            <div style={{ maxWidth:700, position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '60%',}}>
-              <AddProductCard handleChange={this.handleChange}/>
+          <Modal style={{}} open={this.state.open} onClose={this.handleClose}>
+            <div style={{height:'90%', overflowY: 'auto', maxWidth:700, position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '60%',}}>
+              <AddProductCard formSubmit={this.formSubmit} handleChange={this.handleChange} handleCatChange={this.handleCatChange}/>
             </div>
           </Modal>
       </Container>
