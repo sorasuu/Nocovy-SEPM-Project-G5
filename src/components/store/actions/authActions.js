346a27fb978bd0bea8f35c54f8da5d3ca491eb01
Lam Nguyen <topovn0908@gmail.com>
2020-04-26 22:33:06 +0700

    Update authActions.js

--- src/components/store/actions/authActions.js
+++ src/components/store/actions/authActions.js
@@ -33,13 +33,11 @@ export const signUp = (newUser) => {
       return firebase.firestore().collection('users').doc(resp.user.uid).set({
         displayName: newUser.firstName +" "+newUser.lastName,
         email:  newUser.email,
-        password: newUser.password,
         firstName: newUser.firstName,
         lastName: newUser.lastName,
         phoneNumber: newUser.phoneNumber,
         image: newUser.image,
         url: newUser.url,
-        progress: newUser.progress
       });
     }).then(() => {
       dispatch({ type: 'SIGNUP_SUCCESS' });
