4f08f473dda57dffcdd224d7ff52e0e369875a01
sorasuu <37429439+sorasuu@users.noreply.github.com>
2020-05-29 13:11:56 +0700

    Update NotFound.js

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -20,7 +20,7 @@ const useStyles = theme => ({
 const notfound = {
     fontFamily: 'Kanit',
     position: "relative",
-    marginTop: "8%",
+    marginTop: "2%",
     textAlign: 'center',
     lineHeight: "1.4",
 }
@@ -32,7 +32,9 @@ class NotFound extends Component {
 
         return (
             <div style={notfound} >
+                <img style={{height:'30%',maxHeight:'300px'}} alt='' src='https://firebasestorage.googleapis.com/v0/b/sepm-nocovy.appspot.com/o/mirage-page-not-found.png?alt=media&token=de37129c-849e-4cf4-8fb5-c1d768976986' ></img>
                 <h1 style={{fontSize:"190px"}}>404</h1>
+                
                 <h4 style={{fontSize : "30px"}}>Oops! We couldn't find that page</h4>
 
                 <br />
